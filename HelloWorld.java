33f5d8d6ffcccb4608d28a4018a5fa10a92fde97
zhangxiaowei <zhangxiaowei@virtaitech.com>
2026-07-07 19:48:44 +0800

    update: 添加TODO注释

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,6 @@
-    // workspaceB 添加的功能
+    // 1111workspaceB 添加的功能
+    public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+        }
+    }
